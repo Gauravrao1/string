b99ddc3757c26135a472c4ee689fbc7bea8fd50e
Gaurav Rao <gauravraorao6@gmail.com>
2025-09-02 10:44:50 +0530

    finding out lenghth of string

---
+++ strlength.java
@@ -0,0 +1,6 @@
+public class strlength{
+        public static void main(String[] args) {
+                String str ="HRITIK ROSHAN";
+                System.out.println(str.length());
+        }
+}
