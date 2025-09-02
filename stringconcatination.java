51c33417d73751f181df404e0551e2becc889a1d
Gaurav Rao <gauravraorao6@gmail.com>
2025-09-02 10:56:43 +0530

    concatination

---
+++ stringconcatination.java
@@ -0,0 +1,8 @@
+public class stringconcatination {
+
+        public static void main(String[] args) {
+                String str1 ="Gaurav";
+                String str2 ="Rao";
+                String str3 = str1+" "+str2;
+                System.out.println(str3);         }
+}
