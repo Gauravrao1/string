41b9d2e78a1a9eb925ac0df6080ac2e8e1f9f24a
Gaurav Rao <gauravraorao6@gmail.com>
2025-09-02 17:58:37 +0530

    Finding largest string through lexical approch

---
+++ printlargeststr.java
@@ -0,0 +1,16 @@
+public class printlargeststr {
+public static void main(String[] args) {
+ String fruits[] ={"appple","bannana","mango"};  
+ String largest =fruits[0];
+       for(int i=1;i<fruits.length;i++){
+        if(largest.compareTo(fruits[i])<0){
+
+                largest=fruits[i];
+                
+        }
+       } 
+System.out.println(largest);
+}
+        
+
+}
